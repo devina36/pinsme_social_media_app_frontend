b7488a1e7a448fc9a4bdc437fd2810c8d2cbad76
Devina Amalia <devina.amalia36@gmail.com>
2022-11-09 23:32:51 +0700

    readme

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -103,11 +103,11 @@ const UserProfile = () => {
         </div>
 
         {pins?.length === 0 ? (
+          <div className="flex justify-center font-bold items-center w-full text-1xl mt-2">No Pins Found!</div>
+        ) : (
           <div className="px-2">
             <MasonryLayout pins={pins} />
           </div>
-        ) : (
-          <div className="flex justify-center font-bold items-center w-full text-1xl mt-2">No Pins Found!</div>
         )}
       </div>
     </div>
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,13 +1,5 @@
 export const categories = [
   {
-    name: 'cars',
-    image: 'https://i.pinimg.com/750x/eb/47/44/eb4744eaa3b3ccd89749fa3470e2b0de.jpg',
-  },
-  {
-    name: 'fitness',
-    image: 'https://i.pinimg.com/236x/25/14/29/251429345940a47490cc3d47dfe0a8eb.jpg',
-  },
-  {
     name: 'wallpaper',
     image: 'https://i.pinimg.com/236x/03/48/b6/0348b65919fcbe1e4f559dc4feb0ee13.jpg',
   },
@@ -32,20 +24,16 @@ export const categories = [
     image: 'https://i.pinimg.com/736x/f4/e5/ba/f4e5ba22311039662dd253be33bf5f0e.jpg',
   },
   {
+    name: 'anime',
+    image: 'https://i.pinimg.com/564x/e1/3d/a1/e13da15992519e57357896dd77ac2c58.jpg',
+  },
+  {
     name: 'travel',
     image: 'https://i.pinimg.com/236x/fa/95/98/fa95986f2c408098531ca7cc78aee3a4.jpg',
   },
   {
     name: 'quotes',
     image: 'https://i.pinimg.com/236x/46/7c/17/467c17277badb00b638f8ec4da89a358.jpg',
-  },
-  {
-    name: 'cats',
-    image: 'https://i.pinimg.com/236x/6c/3c/52/6c3c529e8dadc7cffc4fddedd4caabe1.jpg',
-  },
-  {
-    name: 'dogs',
-    image: 'https://i.pinimg.com/236x/1b/c8/30/1bc83077e363db1a394bf6a64b071e9f.jpg',
   },
   {
     name: 'others',
