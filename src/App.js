faaebc7588396d8e2e09869d0a356b80cbb5ae9c
Devina Amalia <devina.amalia36@gmail.com>
2022-11-09 09:31:29 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './container/Home';
 import Login from './components/Login';
+import { fetchUser } from './utils/fetchUser';
 
 export const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) navigate('/login');
+  }, []);
   return (
     <Routes>
       <Route path="login" element={<Login />} />
--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -15,7 +15,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
 
   const user = fetchUser();
 
-  const saved = save?.filter((item) => item.postedBy?._id === user?.googleId);
+  const saved = save?.filter((item) => item?.postedBy?._id === user?.googleId);
   const alreadySaved = !!saved?.length;
 
   const savePin = (id) => {
@@ -26,10 +26,10 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
         .insert('after', 'save[-1]', [
           {
             _key: uuidv4(),
-            userId: user.googleId,
+            userId: user?.googleId,
             postedBy: {
               _type: 'postedBy',
-              _ref: user.googleId,
+              _ref: user?.googleId,
             },
           },
         ])
